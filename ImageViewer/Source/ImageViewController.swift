4219358e0063ccbaad03afe82b75138ba60fde2b
Ionut Lucaci <ionutlucaci@gmail.com>
2018-01-09 17:29:41 +0200

    Update ImageViewController.swift
    Support for unsupported types.

--- ImageViewer/Source/ImageViewController.swift
+++ ImageViewer/Source/ImageViewController.swift
@@ -12,3 +12,31 @@ extension UIImageView: ItemView {}
 
 class ImageViewController: ItemBaseController<UIImageView> {
 }
+
+class UnsupportedViewController: ImageViewController {
+    var message: String? {
+        didSet {
+            messageLabel?.text = message
+        }
+    }
+    var messageLabel: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.text = message
+        label.textColor = UIColor.white
+        label.shadowColor = UIColor.black
+        label.shadowOffset = CGSize(width: 1, height: 1)
+        label.textAlignment = .center
+        view.addSubview(label)
+        
+        messageLabel = label
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        messageLabel?.frame = view.bounds
+    }
+}
